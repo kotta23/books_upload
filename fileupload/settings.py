ae7bfccdd069cc38af4f6e53855a677e27acd08e
kotta <engamgadnashaat55@gmail.com>
2020-11-20 12:28:25 +0200

    static file

--- fileupload/settings.py
+++ fileupload/settings.py
@@ -17,7 +17,6 @@ from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -129,7 +128,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mysite/static')
+    os.path.join(BASE_DIR, 'fileupload/static')
 ]
 
 # Media folder settings
